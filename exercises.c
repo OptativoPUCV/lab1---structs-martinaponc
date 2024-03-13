4303e1dc2f4dd4d3384c507ec7b1068c1f5cd6b7
Your Name <you@example.com>
2024-03-13 21:02:14 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,25 +45,7 @@ void reverseArray(int arr[], int size)
     }
 }
 
-int main() 
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    reverseArray(arr, size);
-
-    printf("\nReversed array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
-}
 
 /*
 Ejercicio 3: Filtrar Números Pares
