3f6876f77635d766e35a9dd1701988e289598add
Your Name <you@example.com>
2024-03-13 21:02:52 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -28,7 +28,7 @@ Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-
+void reverseArray(int arr[], int size) 
 
 
 
